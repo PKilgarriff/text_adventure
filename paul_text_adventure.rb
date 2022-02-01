53847940c37ddc3912d9a20d4add496d67fbc2ff
Paul <pkilgarriff@gmail.com>
2022-02-01 14:45:49 +0000

    Correct spelling of 'response'

--- paul_text_adventure.rb
+++ paul_text_adventure.rb
@@ -1,41 +1,3 @@
-# player_alive = true
-# 
-# intro_text = [
-#   "You are standing in a room.",
-#   "There are three doors, one to the left, one to the right, and one in front of you.",
-#   "Choose a door to go through [Type 'Left', 'Right', or 'Forward']"
-# ]
-# werewolf = "A snarling werewolf pounces as you open the door, ripping you apart with its claws."
-# goblin = "A goblin smiles at you as you enter, before drawing a rusty sword and plunging it into your chest."
-# death = "Unfortunately you died - better luck next time!"
-# safety = [
-#   "You close the door behind you as you enter a room that contains no dangers, but does have three more doors.",
-#   "Choose a door to go through [Type 'Left', 'Right', or 'Forward']"
-# ]
-# 
-# puts intro_text
-# safe_choices = 0
-# 
-# while player_alive && (safe_choices < 2)
-#   user_input = gets.chomp
-#   if user_input.downcase == 'left'
-#     puts werewolf
-#     puts death
-#     exit
-#   elsif user_input.downcase == 'right'
-#     puts goblin
-#     puts death
-#     exit
-#   elsif user_input.downcase == 'forward'
-#     puts safety
-#     safe_choices += 1
-#   else
-#     puts "I'm afraid that's not a direction you can go - please choose 'Left', 'Right', or 'Forward'"
-#   end
-# end
-# 
-# puts "You made it out of the dungeon! Safe travels on the way to your next adventure."
-
 # Game Variables
 location_descriptions = {
   'passage': "You are in a scary passage.",
@@ -51,6 +13,7 @@ safe = "full"
 game_running = true
 
 # Player variables
+player_alive = true
 previous_location = ""
 location = "passage"
 inventory = []
@@ -115,7 +78,7 @@ while game_running
     when "look"
       puts "You see a man shackled to the wall."
       puts 'The man asks you "Is it day or night outside?"'
-      reponse = gets.chomp.downcase
+      response = gets.chomp.downcase
       case response
       when "day"
         puts '"That\'s a relief" the man sighs. "You should look at the desk in the study to the east before you leave."'
