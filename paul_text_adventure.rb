2c72bdb46f04b66064d8d553eeb46cc378fb32f5
Paul <pkilgarriff@gmail.com>
2022-02-06 21:09:23 +0000

    Add methods to Safe class

--- paul_text_adventure.rb
+++ paul_text_adventure.rb
@@ -7,85 +7,6 @@ else
 end
 ARGV.clear
 
-# Game Variables
-# 4 digit safe combination generated randomly each run 
-safe_combination = rand(1000...10000)
-safe = "full"
-$game_running = true
-first_loop = true
-
-introduction_text = [
-  "Welcome to the Text Adventure!",
-  "Move around by typing in one of the compass directions and hitting enter.",
-  "You can also type 'look' to get more information about your current location.",
-  "If you get stuck, type 'help' to read this message again, or 'quit' to exit the game.",
-  "----------------"
-]
-
-locations_hash = {
-  'passage': {
-    description: "You are in a dimly-lit passage.",
-    look: Proc.new { puts "The walls drip with an unidentifiable ooze, and there appears to be a cave to the north." },
-    movement: {
-      north: "cave",
-      east: "passage",
-      south: "passage",
-      west: "passage"
-    }
-  },
-  'cave': {
-    description: "You are in a draughty cave.",
-    look: Proc.new { puts "It is brighter in here than in the passage, but not by much. You can make out a ladder on the north wall." },
-    movement: {
-      north: "hall",
-      east: "cave",
-      south: "passage",
-      west: "cave"
-    }
-  },
-  'hall': {
-    description: "You are in a hall with a marble floor.",
-    look: Proc.new { puts "You see three doors: one each to the north, east, and west." },
-    movement: {
-      north: "study",
-      east: "outside",
-      south: "cave",
-      west: "cell"
-    }
-  },
-  'study': {
-    description: "You are in a warm and cosy study.",
-    look: Proc.new { puts "You see a desk with documents on it, and what appears to be a safe underneath the desk." },
-    look_at_desk: "You see a piece of paper that reads, The combination is #{study_safe.request_safe_combination}.",
-    look_at_safe: "You see a sturdy safe firmly attached to the floor, it looks like it needs a 4 digit combination.",
-    movement: {
-      north: "study",
-      east: "study",
-      south: "hall",
-      west: "study"
-    }
-  },
-  'outside': {
-    description: "You emerge into sunlight, and look out across rolling hills.",
-    movement: {
-      north: "outside",
-      east: "outside",
-      south: "outside",
-      west: "hall"
-    }
-  },
-  'cell': {
-    description: "You are in a grimy prison cell.",
-    look: method(:cellInteraction),
-    movement: {
-      north: "cell",
-      east: "hall",
-      south: "cell",
-      west: "cell"
-    }
-  }
-}
-
 class Safe
   POSSIBLE_TREASURE = [
     "a pouch of diamonds",
@@ -135,10 +56,91 @@ class Safe
   end
 end
 
+# Game Variables
+# 4 digit safe combination generated randomly each run 
+safe_combination = rand(1000...10000)
+safe = "full"
+$game_running = true
+first_loop = true
+
+introduction_text = [
+  "Welcome to the Text Adventure!",
+  "Move around by typing in one of the compass directions and hitting enter.",
+  "You can also type 'look' to get more information about your current location.",
+  "If you get stuck, type 'help' to read this message again, or 'quit' to exit the game.",
+  "----------------"
+]
+
+locations_hash = {
+  'passage': {
+    description: "You are in a dimly-lit passage.",
+    look: Proc.new { puts "The walls drip with an unidentifiable ooze, and there appears to be a cave to the north." },
+    identify_ooze: Proc.new { puts "I think we need to talk about the meaning of the word \"unidentifiable\"." },
+    movement: {
+      north: "cave",
+      east: "passage",
+      south: "passage",
+      west: "passage"
+    }
+  },
+  'cave': {
+    description: "You are in a draughty cave.",
+    look: Proc.new { puts "It is brighter in here than in the passage, but not by much. You can make out a ladder on the north wall." },
+    movement: {
+      north: "hall",
+      east: "cave",
+      south: "passage",
+      west: "cave"
+    }
+  },
+  'hall': {
+    description: "You are in a hall with a marble floor.",
+    look: Proc.new { puts "You see three doors: one each to the north, east, and west." },
+    movement: {
+      north: "study",
+      east: "outside",
+      south: "cave",
+      west: "cell"
+    }
+  },
+  'study': {
+    description: "You are in a warm and cosy study.",
+    look: Proc.new { puts "You see a desk with documents on it, and what appears to be a safe underneath the desk." },
+    look_at_desk: Proc.new { puts "You see a piece of paper that reads, The combination is #{study_safe.request_safe_combination}." },
+    #    look_at_desk: "You see a piece of paper that reads, The combination is #{study_safe.request_safe_combination}.",
+    look_at_safe: "You see a sturdy safe firmly attached to the floor, it looks like it needs a 4 digit combination.",
+    movement: {
+      north: "study",
+      east: "study",
+      south: "hall",
+      west: "study"
+    }
+  },
+  'outside': {
+    description: "You emerge into sunlight, and look out across rolling hills.",
+    movement: {
+      north: "outside",
+      east: "outside",
+      south: "outside",
+      west: "hall"
+    }
+  },
+  'cell': {
+    description: "You are in a grimy prison cell.",
+    look: method(:cellInteraction),
+    movement: {
+      north: "cell",
+      east: "hall",
+      south: "cell",
+      west: "cell"
+    }
+  }
+}
+
 study_safe = Safe.new("study")
 # puts "The Safe combination is #{study_safe.request_safe_combination}"
 study_safe.safe_information
-puts study_safe.locked
+puts c
 
 # Player variables
 $player_alive = true
@@ -157,13 +159,13 @@ while $game_running
   previous_location = location
   input = gets.chomp.downcase if location != "outside"
   case input
-  when "quit"
+  when 'quit'
     puts "Bye!"
     # break
     exit
   when 'help'
     puts introduction_text[1..4]
-  when "look"
+  when 'look'
     locations_hash[location.to_sym][:look].()
   end
 
@@ -175,17 +177,18 @@ while $game_running
   end
 
   case location
-  when "study"
-    puts "Safe combination is #{safe_combination}." if debug
+  when 'study'
+    puts "Safe combination is #{study_safesafe_combination}." if debug
     case input
-    when "look at desk"
-      puts locations_hash[location.to_sym][:look_at_desk]
-    when "look at safe"
+    when 'look at desk'
+      locations_hash[location.to_sym][:look_at_desk].()
+    when 'look at safe'
       puts locations_hash[location.to_sym][:look_at_safe]
-    when "enter combination #{safe_combination}"
-      if safe == "full"
+    when "enter combination #{study_safe.request_safe_combination}"
+      unless study_safe.is_empty?
         safe = "empty"
-        inventory.push("diamonds")
+        # inventory.push("diamonds")
+        puts "The safe contains " 
         puts "You see some diamonds in the safe, and put them into your pocket."
       else
         puts "You see where the diamonds were, then remember that they are already in your pocket."
